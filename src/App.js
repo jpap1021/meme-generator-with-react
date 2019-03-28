01cbe8a228cf3f8a169889392ffe866dd6db345d
jpap1021 <juan.arteaga8710@gmail.com>
2019-03-28 18:14:00 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Header.js'
+import Header from './Header.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Init project
+        <Header/>
       </div>
     );
   }
